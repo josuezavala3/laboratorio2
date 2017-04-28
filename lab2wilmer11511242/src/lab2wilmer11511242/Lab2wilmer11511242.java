20c5d7e05d945ffac51711801adffa010295e320
josue <wilmerjosues@yahoo.com>
2017-04-28 13:06:47 -0600

    prueba

---
+++ lab2wilmer11511242/src/lab2wilmer11511242/Lab2wilmer11511242.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2wilmer11511242;
+
+/**
+ *
+ * @author HP
+ */
+public class Lab2wilmer11511242 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
